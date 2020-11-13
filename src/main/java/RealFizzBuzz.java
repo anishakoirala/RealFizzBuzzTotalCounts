8e4316db3b8bdbf91a8444d2597363419a1d8aa2
anishakoirala <74284173+anishakoirala@users.noreply.github.com>
2020-11-12 17:39:12 -0700

    Update RealFizzBuzz.java

--- src/main/java/RealFizzBuzz.java
+++ src/main/java/RealFizzBuzz.java
@@ -1,26 +1,47 @@
 public class RealFizzBuzz
 {
     public RealFizzBuzz(int range)  {
+        int lucky = 0;
+        int fizzbuzz = 0;
+        int buzz = 0;
+        int fizz = 0;
+        int integer = 0;
         if (range <= 0) {
             System.out.println("Range to low, please increase range parm to greater than 0");
             return;
         }
         for (int x = 1;x<=range;x++)
         {
-            if (x%15 == 0)
+            if (Integer.toString(x).contains("3"))
+            {
+                System.out.print("lucky ");
+                lucky++;
+            }
+            else if (x%15 == 0)
             {
                 System.out.print("fizzbuzz ");
+                fizzbuzz++;
             }
             else if (x%5 == 0)
             {
                 System.out.print("buzz ");
+                buzz++;
             }
             else if (x % 3 == 0)
             {
                 System.out.print("fizz ");
+                fizz++;
             }
-            else
+            else {
                 System.out.print(x + " ");
+                integer++;
+            }
         }
+        System.out.println("");
+        System.out.println("fizz: " + fizz);
+        System.out.println("buzz: " + buzz);
+        System.out.println("fizzbuzz: " + fizzbuzz);
+        System.out.println("lucky: " + lucky);
+        System.out.println("integer: " + integer);
     }
 }
