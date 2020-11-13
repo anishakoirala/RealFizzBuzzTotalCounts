8066d99082e546e441ce195c9245c7d44254db78
anishakoirala <74284173+anishakoirala@users.noreply.github.com>
2020-11-12 17:37:19 -0700

    Update RunnerTest.java

--- src/test/java/RunnerTest.java
+++ src/test/java/RunnerTest.java
@@ -16,7 +16,12 @@ class RunnerTest {
     @Test
     void main() {
         RealFizzBuzz fuzz = new RealFizzBuzz(20);
-        Assert.assertEquals("1 2 fizz 4 buzz fizz 7 8 fizz buzz 11 fizz 13 14 fizzbuzz 16 17 fizz 19 buzz"
+        Assert.assertEquals("1 2 lucky 4 buzz fizz 7 8 fizz buzz 11 fizz lucky 14 fizzbuzz 16 17 fizz 19 buzz \n" +
+                        "fizz: 4\n" +
+                        "buzz: 3\n" +
+                        "fizzbuzz: 1\n" +
+                        "lucky: 2\n" +
+                        "integer: 10"
                 , outputStreamCaptor.toString()
                 .trim());
     }
